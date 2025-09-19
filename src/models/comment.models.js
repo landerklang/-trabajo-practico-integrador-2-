580c0e4dd4c264757edafb745c0b4583f49e150f
¨lautaro <shortlauty35@gmail.com>
2025-09-19 16:40:49 -0300

    se corrigio un error que habia con respecto a la crear de los timestamps

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,11 +1,12 @@
 import { model, Types, Schema } from "mongoose";
 
-const CommentSchema = new Schema({
-  content: { type: String, minlengt: 5, maxlengt: 500, require: true },
-  author: { type: Types.ObjectId, ref: "User", require: true },
-  article: { type: Types.ObjectId, ref: "Article" },
-  createdAt: { type: Date },
-  updatedAT: { type: Date },
-});
+const CommentSchema = new Schema(
+  {
+    content: { type: String, minlengt: 5, maxlengt: 500, require: true },
+    author: { type: Types.ObjectId, ref: "User", require: true },
+    article: { type: Types.ObjectId, ref: "Article" },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 export const CommentModels = model("Comment", CommentSchema);
--- src/models/tag.models.js
+++ src/models/tag.models.js
@@ -1,10 +1,11 @@
 import { model, Types, Schema } from "mongoose";
 
-const TagSchema = new Schema({
-  name: { type: String, unique: true, trim: true, require: true },
-  description: { type: String, maxlengh: 200 },
-  createdAt: { type: Date },
-  updatedAT: { type: Date },
-});
+const TagSchema = new Schema(
+  {
+    name: { type: String, unique: true, trim: true, require: true },
+    description: { type: String, maxlengh: 200 },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 export const TagsModels = model("Tags", TagSchema);
